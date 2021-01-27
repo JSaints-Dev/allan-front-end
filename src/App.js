ead77cfaad466b83b47fd4178efaa13103167a63
JSaints <saintsjean.dev@gmail.com>
2021-01-27 01:34:44 -0300

    implemented templete

--- src/App.js
+++ src/App.js
@@ -1,3 +1,25 @@
-const app = () => <div>myApp</div>;
+import { Container, LinearProgress } from "@material-ui/core";
+import Header from "components/header";
+import { lazy, Suspense } from "react";
+import { Route, Switch } from "react-router-dom";
+import { ContainerPage } from "ui";
 
-export default app;
+import * as routes from "routes";
+
+const MainPage = lazy(() => import("pages/main"));
+
+const App = () => {
+  return (
+    <ContainerPage>
+      <Header />
+      <Container>
+        <Suspense fallback={<LinearProgress />}>
+          <Switch>
+            <Route path={routes.HOME} component={MainPage} />
+          </Switch>
+        </Suspense>
+      </Container>
+    </ContainerPage>
+  );
+};
+export default App;
--- src/index.js
+++ src/index.js
@@ -2,10 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { CssBaseline } from "@material-ui/core";
+import { BrowserRouter, Route } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <CssBaseline />
+    <BrowserRouter>
+      <Route component={App} />
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
 );
